e712e4c5ee8e51d861b480dee99806fb832b8d97
Wesley <wesley.bekkers@gmail.com>
2017-10-11 16:14:59 +0200

    Class room gemaakt, unittesten toegevoegd.

--- src/main/java/nl/yacht/lakesideresort/Main.java
+++ src/main/java/nl/yacht/lakesideresort/Main.java
@@ -1,7 +1,18 @@
 package nl.yacht.lakesideresort;
 
+import nl.yacht.lakesideresort.domain.Room;
+
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
+
+        Room room1 = new Room(1, Room.RoomType.BUDGET, Room.RoomSize.THREE_FOUR_PERSON, LocalDateTime.now());
+        Room room2 = new Room(2, Room.RoomType.NORMAL, Room.RoomSize.FIVE_SIX_PERSON, LocalDateTime.now());
+        System.out.println(room1.toString());
+        System.out.println(room2);
+        room1.setRoomNumber(3);
+        System.out.println(room1);
 
         BoatRental rental = new BoatRental();
         rental.rent();
---
+++ src/main/java/nl/yacht/lakesideresort/domain/Room.java
@@ -0,0 +1,64 @@
+package nl.yacht.lakesideresort.domain;
+
+import java.time.LocalDateTime;
+
+public class Room {
+
+    private int roomNumber;
+    private RoomType roomType;
+    private RoomSize roomSize;
+    private LocalDateTime availableFrom;
+
+    public enum RoomType{
+        LUXURY, NORMAL, BUDGET
+    }
+    public enum RoomSize{
+
+        ONE_PERSON, TWO_PERSON, THREE_FOUR_PERSON, FIVE_SIX_PERSON
+    }
+
+    public Room(int roomNumber, RoomType roomType, RoomSize roomSize, LocalDateTime availableFrom) {
+
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.roomSize = roomSize;
+        this.availableFrom = availableFrom;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Room: " +this.roomNumber;
+        s += "\nRoomtype: " +this.roomType;
+        s += "\nRoomsize: " +this.roomSize;
+        s += "\nAvailable from: " +this.availableFrom;
+        return s;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    public RoomSize getRoomSize() {
+        return roomSize;
+    }
+
+    public void setRoomSize(RoomSize roomSize) {
+        this.roomSize = roomSize;
+    }
+    //#For testing purposes
+    public LocalDateTime getAvailableFrom() {
+        return availableFrom;
+    }
+}
