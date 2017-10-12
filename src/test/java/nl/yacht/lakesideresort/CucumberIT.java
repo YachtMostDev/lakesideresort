6dfe8cfd972350001c30c28fb86a2652062052e2
Raymond Loman <raymond@carpago.nl>
2017-10-12 16:48:02 +0200

    more p

--- src/test/java/nl/yacht/lakesideresort/CucumberIT.java
+++ src/test/java/nl/yacht/lakesideresort/CucumberIT.java
@@ -1,9 +1,12 @@
 package nl.yacht.lakesideresort;
 
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(snippets = SnippetType.CAMELCASE)
 public class CucumberIT {
 }
