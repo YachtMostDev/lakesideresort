afc01faeb40e4d61b7f8915edd8a8fcdc0a051cf
Tom <tomliefheid@gmail.com>
2017-10-18 16:38:26 +0200

    add getsingleroom method for roomcontroller and roomrepository

--- src/main/java/nl/yacht/lakesideresort/controller/rest/RoomController.java
+++ src/main/java/nl/yacht/lakesideresort/controller/rest/RoomController.java
@@ -15,6 +15,10 @@ public class RoomController {
     public Iterable<Room> getRooms(){
           return roomRepository.getRooms();
     }
+    @RequestMapping(value="{id}", method = RequestMethod.GET)
+    public Room getSingleRoom(@PathVariable long id){
+    	return roomRepository.getSingleRoom(Long.valueOf(id).intValue());
+    }
 
     @RequestMapping(method = RequestMethod.POST)
     public void insertRoom(@RequestBody Room r){
