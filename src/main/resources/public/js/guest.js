6edf7cc76aa4895fe7c3ca2b2d205f3f1b158223
Sander van Rijswijk <sandervanrijswijk@gmail.com>
2017-10-24 14:32:35 +0200

    Work on guest frontend.

--- src/main/resources/public/js/guest.js
+++ src/main/resources/public/js/guest.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $('#guestTable').DataTable({
         "ajax":{"url":"/api/guest","dataSrc":""},
         "columns": [
-            { "data": "guestNumber" },
             { "data": "firstName" },
             { "data": "surName" },
             { "data": "address" },
@@ -54,3 +53,60 @@ $(document).ready(function(){
         $('#guestTable').DataTable().ajax.reload();
     });
 });
+
+function submitGuest(){
+      var formData = $("#guestForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+      $.ajax({
+          url:"/api/guest",
+          type:"post",
+          data: JSON.stringify(formData),
+          contentType: "application/json; charset=utf-8"
+      });
+      $('#guestTable').DataTable().ajax.reload();
+}
+
+//$("#submitEdit").click(function(e){
+function submitEdit(){
+    var formData = $("#guestForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+    var guestNumber = formData.guestNumber;
+    delete formData.guestNumber;
+    for(var key in formData){
+        if(formData[key] == "" || formData == null) delete formData[key];
+    }
+    $.ajax({
+        url:"/api/guest/" + guestNumber,
+        type:"put",
+        data: JSON.stringify(formData),
+        contentType: "application/json; charset=utf-8"
+    });
+    $('#guestTable').DataTable().ajax.reload();
+}
+
+//$("#submitDelete").click(function(e){
+function submitDelete(){
+    var formData = $("#guestForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+    var guestNumber = formData.guestNumber;
+    $.ajax({
+        url:"/api/guest/" + guestNumber,
+        type:"delete",
+        data: JSON.stringify(formData),
+        contentType: "application/json; charset=utf-8"
+    });
+    $('#guestTable').DataTable().ajax.reload();
+}
+
+function createGuest(){
+    $("#btnsubmit").attr('onclick', 'submitGuest();');
+}
+
+function editGuest(){
+    $("#btnsubmit").attr('onclick', 'submitEdit();');
+}
+
+function deleteGuest(){
+    $("#btnsubmit").attr('onclick', 'submitDelete()');
+}
+
+function refreshTable(){
+
+}
