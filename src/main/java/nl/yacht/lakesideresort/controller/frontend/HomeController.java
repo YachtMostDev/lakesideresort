26bb24d5490895754180063c14e93ca023daf9aa
Kay van Bree <kayvanbree@gmail.com>
2017-10-25 15:33:09 +0200

    Created templates for guest and room pages

--- src/main/java/nl/yacht/lakesideresort/controller/frontend/HomeController.java
+++ src/main/java/nl/yacht/lakesideresort/controller/frontend/HomeController.java
@@ -1,15 +1,25 @@
-package nl.yacht.lakesideresort.controller;
+package nl.yacht.lakesideresort.controller.frontend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/", method= RequestMethod.GET)
+@RequestMapping(value="/")
 public class HomeController {
 
     @RequestMapping(value="", method= RequestMethod.GET)
     public String home(){
         return "index";
+    }
+
+    @RequestMapping(value="/guest", method= RequestMethod.GET)
+    public String guest(){
+        return "guest";
+    }
+
+    @RequestMapping(value="/room", method= RequestMethod.GET)
+    public String room(){
+        return "room";
     }
 }
