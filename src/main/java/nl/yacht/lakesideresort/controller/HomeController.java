8e02fd2a2c594f34c82a71c1100ce98377034fed
Kay van Bree <kayvanbree@gmail.com>
2017-10-25 15:01:31 +0200

    Mustache is working

---
+++ src/main/java/nl/yacht/lakesideresort/controller/HomeController.java
@@ -0,0 +1,15 @@
+package nl.yacht.lakesideresort.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/", method= RequestMethod.GET)
+public class HomeController {
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public String home(){
+        return "index";
+    }
+}
