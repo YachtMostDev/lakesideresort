e03bca8e0c81438ccecd6687e49d0ba0d73f3f63
Kay van Bree <kayvanbree@gmail.com>
2017-10-26 15:18:50 +0200

    Fixed the damned menu

--- src/main/resources/public/js/menu.js
+++ src/main/resources/public/js/menu.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     var pathArray = window.location.pathname.split( '/' );
-    $(".navbar-nav").each(function(i, obj) {
-        obj.removeClass("active");
-        if(obj.Text() == pathArray[0]){
-            obj.addClass("active");
+    var currentPage = pathArray[1].toLowerCase()
+
+    $(".navbar-nav li").each(function(index) {
+        $(this).removeClass("active");
+        var linkText = $(this).children("a").first().attr("href").toLowerCase();
+        var currentLink = linkText.split("/")[1];
+
+        if(currentLink == currentPage){
+            $(this).addClass("active");
         }
     });
 });
