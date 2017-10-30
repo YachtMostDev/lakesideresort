6fcff7d835d40fb6ccc50d5fba82e4936623b818
Tom <tomliefheid@gmail.com>
2017-10-30 14:54:28 +0100

    update package structure add roomManager add unit tests for roomMananager

---
+++ src/main/java/nl/yacht/lakesideresort/controller/RoomController.java
@@ -0,0 +1,54 @@
+package nl.yacht.lakesideresort.controller;
+
+import nl.yacht.lakesideresort.manager.RoomManager;
+import nl.yacht.lakesideresort.repository.RoomRepository;
+import nl.yacht.lakesideresort.domain.Room;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/room")
+public class RoomController{
+
+	//private RoomManager roomManager = new RoomManager(roomRepository);
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private RoomManager roomManager;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Room> getRooms(){
+    	return roomManager.getRooms();
+    }
+
+    /** return room id of found room*/
+    @RequestMapping(value = "/search/{roomnumber}", method = RequestMethod.GET)
+    public long findRoomByRoomNumber(@PathVariable int roomnumber){
+		return roomManager.findRoomByRoomNumber(roomnumber);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.GET)
+    public Room getSingleRoom(@PathVariable long id){
+	    return roomManager.getSingleRoom(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public boolean insertRoom(@RequestBody Room r){
+		return roomManager.insertRoom(r);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public void changeRoom(@PathVariable long id, @RequestBody Room room) {
+    	roomManager.updateRoom(id, room);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void deleteRoom(@PathVariable long id){
+        roomManager.deleteRoom(id);
+    }
+
+
+
+}
