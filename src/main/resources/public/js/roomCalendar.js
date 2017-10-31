4943d042a4a4578a233c31880b7e532c0349f42e
gorgony <n.j.vannispen@gmail.com>
2017-10-31 10:43:06 +0100

    refactored availability added tests

--- src/main/resources/public/js/roomCalendar.js
+++ src/main/resources/public/js/roomCalendar.js
@@ -1,6 +1,5 @@
 var monthDifference = 0;
 var roomId;
-var roomData;
 var monthNames = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
   "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"
 ];
@@ -11,84 +10,60 @@ var monthNames = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
     - creating a get in which you only get the room corresponding with the given id
 **/
 
-function prepareCalendar(id){
-
+function retrieveDataForCalendar(id){
     roomId = id;
+
+    var today = new Date();
+    var startOfMonth = new Date(today.getFullYear(), today.getMonth() + monthDifference, 1, 23);
+    var endOfMonth = new Date(new Date(startOfMonth.getFullYear(), startOfMonth.getMonth() + 1, 1) - 1);
 
     $.ajax({
         type: "GET",
-        url:"/api/booking/",
+        url:"/api/availability/byRoom/" + id + "/" + startOfMonth.toISOString().slice(0,10) + "/" + endOfMonth.toISOString().slice(0,10),
         dataType: 'json',
         async:true,
         success: function(data) {
-            monthDifference = 0;
-            drawCalendar(id, data);
+            drawCalendar(data, startOfMonth, endOfMonth);
         }
     });
 }
 
-function drawCalendar(id, data){
+function drawCalendar(data, startOfMonth, endOfMonth){
     var calendar = document.getElementById("calendar");
     var row = document.createElement("tr");
-    var today = new Date();
 
-    //Calculates days of the month
-    var daysOfMonth = new Date(new Date(today.getFullYear(), today.getMonth() + 1 + monthDifference, 1) -1).getDate();
-    var start = new Date(today.getYear(), today.getMonth()+monthDifference, 2).getDay();
-
-    roomData = data;
-    today.setMonth(today.getMonth()+monthDifference);
-    var currentMonth = today.getMonth()%12;
-
-    document.getElementById("month").innerHTML = monthNames[currentMonth];
-    document.getElementById("year").innerHTML = today.getFullYear();
     deleteRows();
 
-    var roomList = [];
+    document.getElementById("month").innerHTML = monthNames[startOfMonth.getMonth() % 12];
+    document.getElementById("year").innerHTML = "" + startOfMonth.getFullYear();
 
-    if (roomData != null){
-        for (i=0; i < roomData.length; i++){
-            var startDate = (new Date(roomData[i].startDate.year, monthNames.indexOf(roomData[i].startDate.month), roomData[i].startDate.dayOfMonth)).getMonth();
-            var endDate = (new Date(roomData[i].endDate.year, monthNames.indexOf(roomData[i].endDate.month), roomData[i].endDate.dayOfMonth)).getMonth();
-            var month = today.getMonth();
+    var dateArray = [];
 
-            if (roomData[i].room.id == id){
-                if (startDate <= month && endDate >= month){
-                    console.log(roomData[i]);
-                    roomList.push(roomData[i]);
-                }
-            }
-        }
+    for(var forStartOfMonth = new Date(startOfMonth); forStartOfMonth <= endOfMonth; forStartOfMonth.setDate(forStartOfMonth.getDate()+1)){
+        dateArray.push(new Date(forStartOfMonth));
     }
 
-    if(roomList != []){
-        console.log(roomList);
-    }
-
-    for (var index = 1; index-start <= daysOfMonth; index++){
+    var count = 0;
+    var temp = ((startOfMonth.getDay() == 0)?7:startOfMonth.getDay())*-1+1;
+    for (var index = temp; index < endOfMonth.getDate(); index++){
+        count++;
         var td = document.createElement("td");
         var a = document.createElement("a");
         var div = document.createElement("div");
-        if (index - start > 0){
-            for(i=0; i < roomList.length; i++){
-                var startDate = new Date(roomData[i].startDate.year, monthNames.indexOf(roomData[i].startDate.month), roomData[i].startDate.dayOfMonth);
-                var endDate = new Date(roomData[i].endDate.year, monthNames.indexOf(roomData[i].endDate.month), roomData[i].endDate.dayOfMonth);
-                var currentDate = new Date(today.getFullYear(), today.getMonth(), index-start);
-                if(startDate <= currentDate && endDate >= currentDate){
-                    td.className = "red";
-                    a.href = "#";
-                }
+        if (index >= 0){
+            if(data[dateArray[index].toISOString().slice(0,10)]){
+                td.style.backgroundColor = "red";
+                a.style.color = "white";
             }
+            div.innerHTML = dateArray[index].getDate();
+        } else {
+            div.innerHTML = "";
         }
         row.appendChild(td);
         td.appendChild(a);
         div.className = "fullSize";
-        div.innerHTML = (index - start < 1)? "": index-start;
-        if (index - start < 1){
-            td.className = "unavailable";
-        }
         a.appendChild(div);
-        if(index % 7 == 0){
+        if(count % 7 == 0){
            row.className = "tableRow";
            calendar.appendChild(row);
            row = document.createElement("tr");
@@ -104,10 +79,10 @@ function deleteRows(){
 
 function lastMonth(){
     monthDifference--;
-    drawCalendar(roomId, roomData);
+    retrieveDataForCalendar(roomId);
 }
 
 function nextMonth(){
     monthDifference++;
-    drawCalendar(roomId, roomData);
+    retrieveDataForCalendar(roomId);
 }
---
+++ src/test/java/nl/yacht/lakesideresort/manager/AvailabilityManagerTest.java
@@ -0,0 +1,160 @@
+package nl.yacht.lakesideresort.manager;
+
+import nl.yacht.lakesideresort.controller.BookingRepository;
+import nl.yacht.lakesideresort.controller.RoomRepository;
+import nl.yacht.lakesideresort.domain.Booking;
+import nl.yacht.lakesideresort.domain.Guest;
+import nl.yacht.lakesideresort.domain.Room;
+import org.apache.tomcat.jni.Local;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by njvan on 30-Oct-17.
+ */
+public class AvailabilityManagerTest {
+    BookingRepository bookingRepository;
+    RoomRepository roomRepository;
+    AvailabilityManager availabilityManager;
+
+    @Before
+    public void setup(){
+        ArrayList<Booking> bookingList = new ArrayList<>();
+        ArrayList<Room> roomList = new ArrayList<>();
+
+        LocalDate date1 = LocalDate.parse("2017-10-01");
+        LocalDate date2 = LocalDate.parse("2017-10-04");
+        LocalDate date3 = LocalDate.parse("2017-10-09");
+        LocalDate date4 = LocalDate.parse("2017-10-12");
+        LocalDate date5 = LocalDate.parse("2017-10-17");
+        LocalDate date6 = LocalDate.parse("2017-10-20");
+
+        Guest guest = new Guest();
+        Room room1 = new Room(101, Room.RoomType.BUDGET, Room.RoomSize.ONE_PERSON,date1);
+        Room room2 = new Room(102, Room.RoomType.BUDGET, Room.RoomSize.ONE_PERSON,date1);
+        Room room3 = new Room(103, Room.RoomType.BUDGET, Room.RoomSize.ONE_PERSON,date1);
+        Room room4 = new Room(104, Room.RoomType.BUDGET, Room.RoomSize.ONE_PERSON,date1);
+
+        roomList.add(room1);
+        roomList.add(room2);
+        roomList.add(room3);
+        roomList.add(room4);
+
+        bookingList.add(new Booking(guest,room1,date1,date2));
+        bookingList.add(new Booking(guest,room1,date3,date4));
+        bookingList.add(new Booking(guest,room1,date5,date6));
+        bookingList.add(new Booking(guest,room2,date2,date3));
+        bookingList.add(new Booking(guest,room2,date4,date5));
+        bookingList.add(new Booking(guest,room3,date1,date3));
+        bookingList.add(new Booking(guest,room3,date4,date6));
+        bookingList.add(new Booking(guest,room4,date3,date6));
+
+        bookingRepository = mock(BookingRepository.class);
+        roomRepository = mock(RoomRepository.class);
+        availabilityManager = new AvailabilityManager(bookingRepository, roomRepository);
+
+        when(bookingRepository.findAll()).thenReturn(bookingList);
+        when(roomRepository.findAll()).thenReturn(roomList);
+        when(roomRepository.findOne(new Long("101"))).thenReturn(room1);
+        when(roomRepository.findOne(new Long("102"))).thenReturn(room2);
+        when(roomRepository.findOne(new Long("103"))).thenReturn(room3);
+        when(roomRepository.findOne(new Long("104"))).thenReturn(room4);
+    }
+
+    @Test
+    public void getAvailabilityOfRoom() throws Exception {
+        HashMap<String, Boolean> expectedResult = new HashMap<>();
+        expectedResult.put("2017-10-05", false);
+        expectedResult.put("2017-10-06", false);
+        expectedResult.put("2017-10-07", false);
+        expectedResult.put("2017-10-08", false);
+        expectedResult.put("2017-10-09", true);
+        expectedResult.put("2017-10-10", true);
+        expectedResult.put("2017-10-11", true);
+        expectedResult.put("2017-10-12", true);
+        expectedResult.put("2017-10-13", false);
+        expectedResult.put("2017-10-14", false);
+        expectedResult.put("2017-10-15", false);
+
+        LocalDate date1 = LocalDate.parse("2017-10-05");
+        LocalDate date2 = LocalDate.parse("2017-10-15");
+
+        HashMap<String, Boolean> result = availabilityManager.getAvailabilityOfRoom("101",date1,date2);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void getAvailabilityOnDate() throws Exception {
+        HashMap<String, Boolean> expectedResult = new HashMap<>();
+        expectedResult.put("101", false);
+        expectedResult.put("102", true);
+        expectedResult.put("103", true);
+        expectedResult.put("104", false);
+
+        LocalDate date = LocalDate.parse("2017-10-05");
+        HashMap<String, Boolean> result = availabilityManager.getAvailabilityOnDate(date);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void getAvailabilityOnDate2() throws Exception {
+        HashMap<String, Boolean> expectedResult = new HashMap<>();
+        expectedResult.put("101", true);
+        expectedResult.put("102", true);
+        expectedResult.put("103", true);
+        expectedResult.put("104", true);
+
+        LocalDate date = LocalDate.parse("2017-10-17");
+        HashMap<String, Boolean> result = availabilityManager.getAvailabilityOnDate(date);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void getAvailabilityOfAllRooms() throws Exception {
+        HashMap<String, HashMap<String, Boolean>> expectedResult = new HashMap<>();
+
+        HashMap<String, Boolean> map1 = new HashMap<>();
+        map1.put("2017-10-11", true);
+        map1.put("2017-10-12", true);
+        map1.put("2017-10-13", false);
+
+        HashMap<String, Boolean> map2 = new HashMap<>();
+        map2.put("2017-10-11", false);
+        map2.put("2017-10-12", true);
+        map2.put("2017-10-13", true);
+
+        HashMap<String, Boolean> map3 = new HashMap<>();
+        map3.put("2017-10-11", false);
+        map3.put("2017-10-12", true);
+        map3.put("2017-10-13", true);
+
+        HashMap<String, Boolean> map4 = new HashMap<>();
+        map4.put("2017-10-11", true);
+        map4.put("2017-10-12", true);
+        map4.put("2017-10-13", true);
+
+        expectedResult.put("101", map1);
+        expectedResult.put("102", map2);
+        expectedResult.put("103", map3);
+        expectedResult.put("104", map4);
+
+        LocalDate date1 = LocalDate.parse("2017-10-11");
+        LocalDate date2 = LocalDate.parse("2017-10-13");
+
+        HashMap<String, HashMap<String, Boolean>> result = availabilityManager.getAvailabilityOfAllRooms(date1,date2);
+
+        assertEquals(expectedResult, result);
+    }
+
+}
