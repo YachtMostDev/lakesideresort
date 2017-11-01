9ca97745912b673ed36f341fe68355a3d6e58a14
gorgony <n.j.vannispen@gmail.com>
2017-11-01 14:40:20 +0100

    rename for fix

--- src/main/java/nl/yacht/lakesideresort/WebSocketConfig.java
+++ src/main/java/nl/yacht/lakesideresort/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig_old extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
